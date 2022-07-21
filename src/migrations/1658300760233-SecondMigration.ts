b4bb8fcf41300dafb49d75a1d68ee73a44e6ea65
MinhTuanVo1 <tuan.vo@zodinet.tech>
2022-07-21 09:33:28 +0700

    fix: change port in file main.ts

--- src/migrations/1658300760233-SecondMigration.ts
+++ src/migrations/1658300760233-SecondMigration.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class SecondMigration1658300760233 implements MigrationInterface {
-    name = 'SecondMigration1658300760233'
+  name = 'SecondMigration1658300760233';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "avatar" character varying NOT NULL DEFAULT 'https://i.pinimg.com/564x/b3/48/ac/b348acfeb9a26afe01022ec26550bda3.jpg'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "avatar" character varying NOT NULL DEFAULT 'https://i.pinimg.com/564x/b3/48/ac/b348acfeb9a26afe01022ec26550bda3.jpg'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "avatar"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "avatar"`);
+  }
 }
